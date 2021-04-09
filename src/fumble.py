9c5fb435ee5cd74f232b39d0cc7695573e337dd4
davidbradlycurtis <s531274@nwmissouri.edu>
2021-04-09 17:55:11 -0500

    Use args in function calls

--- src/fumble.py
+++ src/fumble.py
@@ -9,47 +9,47 @@ import config
 #############
 # Argparser #
 #############
-
 parser = argparse.ArgumentParser(description='Fumble Buddy! Execute this script to randomly determine critical miss/hits automatically.')
-parser.add_argument('--repeat', type=int, required=False, help='number of situations to generate')
 
-subparsers = parser.add_subparsers()
+subparsers = parser.add_subparsers(dest='command')
 parser_hit = subparsers.add_parser('hit', help='generate a critical hit situation')
 hit_group = parser_hit.add_mutually_exclusive_group(required=True)
-hit_group.add_argument('--b', action='store_true', required=False, help='bludgeoning damage')
-hit_group.add_argument('--s', action='store_true', required=False, help='slashing damage')
-hit_group.add_argument('--p', action='store_true', required=False, help='piercing damage')
-hit_group.add_argument('--poison', required=False, action='store_true', help='poison damage')
-hit_group.add_argument('--a', action='store_true', required=False, help='acid damage')
-hit_group.add_argument('--f', action='store_true', required=False, help='fire damage')
-hit_group.add_argument('--c', action='store_true', required=False, help='cold damage')
-hit_group.add_argument('--r', action='store_true', required=False, help='radiant damage')
-hit_group.add_argument('--l', action='store_true', required=False, help='lightning damage')
-hit_group.add_argument('--t', action='store_true', required=False, help='thunder damage')
-hit_group.add_argument('--force', action='store_true', required=False, help='force damage')
-hit_group.add_argument('--n', action='store_true', required=False, help='necrotic damage')
-hit_group.add_argument('--psychic', action='store_true', required=False, help='psychic damage')
+hit_group.add_argument('-b', action='store_true', required=False, help='bludgeoning damage')
+hit_group.add_argument('-s', action='store_true', required=False, help='slashing damage')
+hit_group.add_argument('-p', action='store_true', required=False, help='piercing damage')
+hit_group.add_argument('-poison', required=False, action='store_true', help='poison damage')
+hit_group.add_argument('-a', action='store_true', required=False, help='acid damage')
+hit_group.add_argument('-f', action='store_true', required=False, help='fire damage')
+hit_group.add_argument('-c', action='store_true', required=False, help='cold damage')
+hit_group.add_argument('-r', action='store_true', required=False, help='radiant damage')
+hit_group.add_argument('-l', action='store_true', required=False, help='lightning damage')
+hit_group.add_argument('-t', action='store_true', required=False, help='thunder damage')
+hit_group.add_argument('-force', action='store_true', required=False, help='force damage')
+hit_group.add_argument('-n', action='store_true', required=False, help='necrotic damage')
+hit_group.add_argument('-psychic', action='store_true', required=False, help='psychic damage')
 
 parser_miss = subparsers.add_parser('miss', help='generate a critical miss situation')
 miss_group = parser_miss.add_mutually_exclusive_group(required=True)
-miss_group.add_argument('--b', action='store_true', required=False, help='bludgeoning damage')
-miss_group.add_argument('--s', action='store_true', required=False, help='slashing damage')
-miss_group.add_argument('--p', action='store_true', required=False, help='piercing damage')
-miss_group.add_argument('--poison', required=False, action='store_true', help='poison damage')
-miss_group.add_argument('--a', action='store_true', required=False, help='acid damage')
-miss_group.add_argument('--f', action='store_true', required=False, help='fire damage')
-miss_group.add_argument('--c', action='store_true', required=False, help='cold damage')
-miss_group.add_argument('--r', action='store_true', required=False, help='radiant damage')
-miss_group.add_argument('--l', action='store_true', required=False, help='lightning damage')
-miss_group.add_argument('--t', action='store_true', required=False, help='thunder damage')
-miss_group.add_argument('--force', action='store_true', required=False, help='force damage')
-miss_group.add_argument('--n', action='store_true', required=False, help='necrotic damage')
-miss_group.add_argument('--psychic', action='store_true', required=False, help='psychic damage')
+miss_group.add_argument('-b', action='store_true', required=False, help='bludgeoning damage')
+miss_group.add_argument('-s', action='store_true', required=False, help='slashing damage')
+miss_group.add_argument('-p', action='store_true', required=False, help='piercing damage')
+miss_group.add_argument('-poison', required=False, action='store_true', help='poison damage')
+miss_group.add_argument('-a', action='store_true', required=False, help='acid damage')
+miss_group.add_argument('-f', action='store_true', required=False, help='fire damage')
+miss_group.add_argument('-c', action='store_true', required=False, help='cold damage')
+miss_group.add_argument('-r', action='store_true', required=False, help='radiant damage')
+miss_group.add_argument('-l', action='store_true', required=False, help='lightning damage')
+miss_group.add_argument('-t', action='store_true', required=False, help='thunder damage')
+miss_group.add_argument('-force', action='store_true', required=False, help='force damage')
+miss_group.add_argument('-n', action='store_true', required=False, help='necrotic damage')
+miss_group.add_argument('-psychic', action='store_true', required=False, help='psychic damage')
 
 parser_injury = subparsers.add_parser('injury', help='generate a random injury')
 injury_group = parser_injury.add_mutually_exclusive_group(required=True)
-injury_group.add_argument('--m', action='store_true', required=False, help='minor injury')
-injury_group.add_argument('--s', action='store_true', required=False, help='sever injury')
+injury_group.add_argument('-m', action='store_true', required=False, help='minor injury')
+injury_group.add_argument('-s', action='store_true', required=False, help='sever injury')
+
+parser.add_argument('--repeat', type=int, default=None, help='number of situations to generate')
 
 args = parser.parse_args()
 
@@ -57,28 +57,11 @@ args = parser.parse_args()
 # Functions #
 #############
 
-def run(args):
-    '''
-    The main method containing fumble logic
+def repeat(fun, args):
+    [fun(args) for _ in range(args.repeat)]
 
-    '''
-    if args.choice == 'miss':
-        if rollDie(10) > 4:
-            miss_picker()
-        else:
-            print(config.NORMAL_MISS)
-    elif args.choice == 'hit':
-        if rollDie(10) > 4:
-            hit_picker()
-        else:
-            print(config.NORMAL_HIT)
-    elif args.choice == 'injury':
-        injury()
-
-def hit_picker():
-    damage_type = input('Select damage type:')
-
-    if (damage_type == '1' or damage_type.lower() == 'b' or damage_type.lower() == 'bludgeoning'):
+def hit_picker(args):
+    if (args.b):
         roll = rollDie(100)
         if roll < 45:
             print(config.DISARM_PRONE[listPicker(5)])
@@ -92,7 +75,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.MAJOR_INJURY[7])
 
-    elif (damage_type == '2' or damage_type.lower() == 's' or damage_type.lower() == 'slashing'):
+    elif (args.s):
         roll = rollDie(100)
         if roll < 45:
             print(config.DISARM_PRONE[listPicker(5)])
@@ -106,7 +89,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.CRITICAL_WOUND)
 
-    elif (damage_type == '3' or damage_type.lower() == 'p' or damage_type.lower() == 'piercing'):
+    elif (args.p):
         roll = rollDie(100)
         if roll < 45:
             print(config.DISARM_PRONE[listPicker(5)])
@@ -121,7 +104,7 @@ def hit_picker():
             print(config.PIERCING_INJURY[2])
             print(config.PIERCING_SHOT)
 
-    elif (damage_type == '4' or damage_type.lower() == 'poison'):
+    elif (args.poison):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -135,7 +118,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.SHOCK)
 
-    elif (damage_type == '5' or damage_type.lower() == 'a' or damage_type.lower() == 'acid'):
+    elif (args.a):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -149,7 +132,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.ACID_BATH)
 
-    elif (damage_type == '6' or damage_type.lower() == 'f' or damage_type.lower() == 'fire'):
+    elif (args.f):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -163,7 +146,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.ADDITIONAL_FIRE)
 
-    elif (damage_type == '7' or damage_type.lower() == 'c' or damage_type.lower() == 'cold'):
+    elif (args.c):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -177,7 +160,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.FROZEN_SOLID)
 
-    elif (damage_type == '8' or damage_type.lower() == 'r' or damage_type.lower() == 'radiant'):
+    elif (args.r):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -191,7 +174,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.RIGHTEOUS_MARK)
 
-    elif (damage_type == '9' or damage_type.lower() == 'l' or damage_type.lower() == 'lightning'):
+    elif (args.l):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -206,7 +189,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.CHAIN_LIGHTNING)
 
-    elif (damage_type == '10' or damage_type.lower() == 't' or damage_type.lower() == 'thunder'):
+    elif (args.t):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -219,7 +202,7 @@ def hit_picker():
         else:
             print(config.SUPER_CRITICAL)
 
-    elif (damage_type == '11' or damage_type.lower() == 'force'):
+    elif (args.force):
         roll = rollDie(100)
         if roll < 45:
             print(config.DISARM_PRONE[listPicker(5)])
@@ -233,7 +216,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.MAJOR_INJURY[9])
 
-    elif (damage_type == '12' or damage_type.lower() == 'n' or damage_type.lower() == 'necrotic'):
+    elif (args.n):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -247,7 +230,7 @@ def hit_picker():
             print(config.SUPER_CRITICAL)
             print(config.PUTREFY)
 
-    elif (damage_type == '13' or damage_type.lower() == 'psychic'):
+    elif (args.psychic):
         roll = rollDie(100)
         if roll < 45:
             print(config.NORMAL_HIT)
@@ -260,16 +243,9 @@ def hit_picker():
         else:
             print(config.SUPER_CRITICAL)
             print(config.SUICIDAL)
-    else:
-        print('')
-        print('Invalid damage type, please try again.')
-        print('')
-        hit_picker()
 
-def miss_picker():
-    damage_type = input('Select damage type:')
-
-    if (damage_type == '1' or damage_type.lower() == 'b' or damage_type.lower() =='bludgeoning' or damage_type == '2' or damage_type.lower() == 's' or damage_type.lower() =='slashing' or damage_type == '3' or damage_type.lower() == 'p' or damage_type.lower() =='piercing'):
+def miss_picker(args):
+    if (args.p or args.s or args.b):
         roll = rollDie(100)
         if roll < 45:
             print(config.DISARM_PRONE_MISS[listPicker(5)])
@@ -282,7 +258,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '2' or damage_type.lower() == 'poison'):
+    elif (args.poison):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -291,7 +267,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '3' or damage_type.lower() == 'a' or damage_type.lower() == 'acid'):
+    elif (args.a):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -300,7 +276,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '4' or damage_type.lower() == 'f' or damage_type.lower() == 'fire'):
+    elif (args.f):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -309,7 +285,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '5' or damage_type.lower() == 'c' or damage_type.lower() == 'cold'):
+    elif (args.c):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -318,7 +294,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '6' or damage_type.lower() == 'r' or damage_type.lower() == 'radiant'):
+    elif (args.r):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -327,7 +303,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '7' or damage_type.lower() == 'l' or damage_type.lower() == 'lightning'):
+    elif (args.l):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -336,7 +312,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '8' or damage_type.lower() == 't' or damage_type.lower() == 'thunder'):
+    elif (args.t):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -345,7 +321,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '9' or damage_type.lower() == 'force'):
+    elif (args.force):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -354,7 +330,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '10' or damage_type.lower() == 'n' or damage_type.lower() == 'necrotic'):
+    elif (args.n):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -363,7 +339,7 @@ def miss_picker():
         else:
             print(config.SECOND_CHANCE)
 
-    elif (damage_type == '11' or damage_type.lower() == 'psychic'):
+    elif (args.psychic):
         roll = rollDie(10)
         if roll < 6:
             print(config.END_ACTION)
@@ -371,35 +347,43 @@ def miss_picker():
             print(config.IN_YOUR_HEAD)
         else:
             print(config.SECOND_CHANCE)
-    else:
-        print('')
-        print('Invalid damage type, please try again.')
-        print('')
-        miss_picker()
 
-def injury():
-    print('')
-    print('Injury Severity                     ')
-    print('1) (M)inor injury                   ')
-    print('2) (S)evere injury                  ')
-    print('')
-    injuryType = input('Select a minor or severe injury: ')
-    print('')
-
-    if (injuryType.lower() == 'minor' or injuryType.lower() == 'm' or injuryType == '1'):
+def injury(args):
+    if (args.m):
         print(config.MINOR_INJURY[listPicker(12)])
-    elif (injuryType.lower() == 'severe' or injuryType.lower() == 's' or injuryType == '2'):
+    elif (args.s):
         print(config.MAJOR_INJURY[listPicker(11)])
-    else:
-        print('')
-        print('Invalid input, please try again')
-        injury()
 
 def rollDie(die):
     return random.randint(1, die)
 
 def listPicker(count):
     return random.randint(0, count)
+
+def run(args):
+    '''
+    The main method containing fumble logic
+
+    '''
+    if args.command == 'miss':
+        if args.repeat:
+            repeat(miss_picker, args)
+        elif rollDie(10) > 4:
+            miss_picker(args)
+        else:
+            print(config.NORMAL_MISS)
+    elif args.command == 'hit':
+        if args.repeat:
+            repeat(hit_picker, args)
+        elif rollDie(10) > 4:
+            hit_picker(args)
+        else:
+            print(config.NORMAL_HIT)
+    elif args.command == 'injury':
+        if args.repeat:
+            repeat(injury, args)
+        else:
+            injury(args)
 
 #############
 #   Body    #
